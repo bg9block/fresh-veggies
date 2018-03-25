bb5a18eeda24c172fe04506fbdfa5c7b113ac982
Ilia Syrtsou <i_l_i_a@Ilias-MacBook-Pro.local>
2018-03-25 21:12:37 +0300

    bootstrap asp.net core 2.0 web api project

---
+++ WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class CartController : Controller
+    {
+        // GET api/cart/total
+        [HttpGet("total")]
+        public string GetTotalFor(List<Guid> productIds, List<Guid> voucherIds)
+        {
+            return "value";
+        }
+    }
+}
